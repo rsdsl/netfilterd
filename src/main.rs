f5ed701426d34679ce744a98bf98241d5441d162
HimbeerserverDE <himbeerserverde@gmail.com>
2023-03-21 15:54:23 +0100

    enable NAT

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ipt = iptables::new(false)?;
+
+    ipt.append("nat", "POSTROUTING", "-o rsppp0 -j MASQUERADE")?;
+
+    Ok(())
 }
